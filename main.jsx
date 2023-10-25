035612fda8ecea7fdd0da25243ed90fddf845792
Tachito03 <arellanos.baaeus@gmail.com>
2023-10-25 13:38:23 -0600

    readme

--- main.jsx
+++ main.jsx
@@ -1,4 +1,7 @@
+/* Importamos react-dom */
 import { createRoot } from 'react-dom/client';
 import { App } from './src/App';
+
+//Renderizado de la app
 const app = createRoot(document.getElementById('app'));
 app.render(<App />);
